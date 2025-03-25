25e7760367fcafec7004515eefe0aaf55ff608c1
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-03-25 12:27:58 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -54,7 +54,12 @@ if password == st.secrets["acceso"]["clave"]:
 
         if dataset_opcion == "Clientes":
             st.subheader("🧍‍♂️ Exploración de Clientes")
-            st.markdown("- Edad promedio: 42 años.\n- Mayoría en provincias como Buenos Aires, Córdoba y Santa Fe.\n- Conclusión: los clientes se concentran en zonas urbanas con fuerte potencial de segmentación.")
+            st.markdown("✅ Conclusiones preliminares del análisis del dataset Clientes:
+- Edad promedio de los clientes es de 40 años, con una alta concentración entre los 25 y 55.
+- Hay una clara concentración geográfica en el AMBA, especialmente Ciudad de Buenos Aires.
+- El 100% de los clientes están activos (no hay marca de baja).
+- La diversidad de localidades es grande (527), pero unas pocas concentran la mayoría.
+- La base de clientes parece limpia y homogénea, con pocos outliers.")
 
             df_clientes = pd.read_csv("Clientes_transformados.csv")
 
